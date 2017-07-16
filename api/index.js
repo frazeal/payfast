a19ed1254daff553c1378fa79b5444f7a809959c
frazeal <aaalves.francisco@gmail.com>
2017-07-15 23:12:50 -0300

    Install nodemon

--- api/index.js
+++ api/index.js
@@ -2,10 +2,10 @@ var express = require('express');
 var app = express();
 
 app.listen(3000, function(){
-    console.log('Server running at port:3000.');
+    console.log('Server running at http://localhost:3000.');
 });
 
 app.get('/test', function(request, response) {
-    console.log('Request received.');
+    console.log('Request received at http://localhost:3000.');
     response.send('OK.');
 });
