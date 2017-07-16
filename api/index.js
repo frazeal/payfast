c659dd5d2a30628e1a8361d7f976d4b0ae5a4b38
frazeal <aaalves.francisco@gmail.com>
2017-07-15 22:50:41 -0300

    Implement GET/test

--- api/index.js
+++ api/index.js
@@ -2,5 +2,10 @@ var express = require('express');
 var app = express();
 
 app.listen(3000, function(){
-    console.log('Servidor rodando na porta 3000.');
+    console.log('Server running at port:3000.');
+});
+
+app.get('/test', function(request, response) {
+    console.log('Request received.');
+    response.send('OK.');
 });
